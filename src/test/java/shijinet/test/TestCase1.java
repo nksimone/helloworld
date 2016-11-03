08c35cbb85e68308d4bc0a1f0d749625a81489db
chenyj-vm\chenyj <nksimone@gmail.com>
2016-11-03 18:32:48 +0800

    更改testcase

---
+++ src/test/java/shijinet/test/TestCase1.java
@@ -0,0 +1,14 @@
+package shijinet.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCase1 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/shijinet/test/TestCase2.java
@@ -0,0 +1,29 @@
+package shijinet.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCase2 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented 1");
+	}
+
+	@Test
+	public void test2() {
+		fail("Not yet implemented 2");
+	}
+
+	@Test
+	public void test3() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void test4() {
+		assertTrue(true);
+	}
+
+}
